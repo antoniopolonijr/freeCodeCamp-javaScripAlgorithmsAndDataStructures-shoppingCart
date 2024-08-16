d5afe61f192e76bb163b3a3cd4e22a21afc35a7a
Antonio Poloni <antoniopolonijr@gmail.com>
2024-08-16 15:07:35 -0300

    Implement features in the ShoppingCart method to count items, calculate the taxes and total

--- script.js
+++ script.js
@@ -123,19 +123,37 @@ class ShoppingCart {
     // You now have a small bug. When you try to access a property of an object and the property doesn't exist, you get undefined. This means if the dessert isn't already present in the totalCountPerProduct object, you end up trying to add 1 to undefined, which results in NaN. To fix this, you can use the || operator to set the value to 0 if it doesn't exist.
     const currentProductCount = totalCountPerProduct[product.id]; // to get prepared to update the display with the new product the user added.
     const currentProductCountSpan = document.getElementById(
-      `product-count-for-id${product.id}`
+      `product-count-for-id${id}`
     ); // You haven't written the code to generate the HTML yet, but if a product has already been added to the user's cart then there will be a matching element which you'll need.
     currentProductCount > 1
       ? (currentProductCountSpan.textContent = `${currentProductCount}x`)
       : (productsContainer.innerHTML += `
       <div id="dessert${id}" class="product">
         <p>
-          <span class="product-count" id="product-count-for-id${id}"><span>${name}
+          <span class="product-count" id="product-count-for-id${id}"></span>${name}
         </p>
         <p>${price}</p>
       </div>
       `); // to add new HTML to your productsContainer
     // The behaviour of the addItem method needs to change if the product is already in the cart or not. Use undefined for both the truthy and falsy expressions to avoid a syntax error.
+  }
+  getCounts() {
+    return this.items.length; // You need a way to access the total number of items in the cart. The best way to do this is to add another method to your ShoppingCart class, rather than accessing the items array directly.
+  }
+  calculateTotal() {
+    const subTotal = this.items.reduce((total, item) => total + item.price, 0); // to update the total price of the cart when the user adds an item.
+    const tax = this.calculateTaxes(subTotal);
+    this.total = subTotal + tax;
+    cartSubTotal.textContent = `$${subTotal.toFixed(2)}`; //  to update the HTML in this method as well
+    cartTaxes.textContent = `$${tax.toFixed(2)}`;
+    cartTotal.textContent = `$${this.total.toFixed(2)}`;
+    return this.total;
+  }
+  calculateTaxes(amount) {
+    // Part of the total cost will include the tax, so you need to calculate that as well.
+    return parseFloat(((this.taxRate / 100) * amount).toFixed(2)); // divided by 100, to convert it to a percent
+    // Because of the way computers store and work with numbers, calculations involving decimal numbers can result in some strange behavior. For example, 0.1 + 0.2 is not equal to 0.3. This is because computers store decimal numbers as binary fractions, and some binary fractions cannot be represented exactly as decimal fractions. We want to clean up the number result from your calculation. the .toFixed() method the number 2 as an argument. This will round the number to two decimal places and return a string.
+    // The issue with .toFixed() returning a string is that you want to be able to perform calculations with the tax rate. To fix this, you can pass the .toFixed() call (including the calculation) to the parseFloat() function. This will convert the fixed string back into a number, preserving the existing decimal places.
   }
 }
 
@@ -146,6 +164,8 @@ const addToCartBtns = document.getElementsByClassName("add-to-cart-btn"); // to 
   // You need to iterate through the buttons in your addToCartBtns variable. However, .getElementsByClassName() returns a Collection, which does not have a forEach method. Use the spread operator on the addToCartBtns variable to convert it into an array. Then, use the forEach method to iterate through the array
   btn.addEventListener("click", (event) => {
     cart.addItem(Number(event.target.id), products); // Remember that the id here will be a string, so you need to convert it to a number.
+    totalNumberOfItems.textContent = cart.getCounts(); // update the total number of items on the webpage
+    cart.calculateTotal();
   });
 });
 
